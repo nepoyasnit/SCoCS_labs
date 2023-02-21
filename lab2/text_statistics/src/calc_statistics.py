8ec8415c5d6e0a85206dcbc86a4ba99f0a85407d
Maxim735 <maksiksay@gmail.com>
2023-02-21 18:54:10 +0300

    add another sentences counter

--- lab2/text_statistics/src/calc_statistics.py
+++ lab2/text_statistics/src/calc_statistics.py
@@ -4,3 +4,9 @@ import re
 def amount_of_sentences(text: str) -> int:
     regex = r'([.!?] )'
     return len([*re.finditer(regex, text)])
+
+
+def amount_of_non_declarative_sentences(text: str) -> int:
+    regex = r'([!?] )'
+    return len([*re.finditer(regex, text)])
+
--- lab2/text_statistics/src/main.py
+++ lab2/text_statistics/src/main.py
@@ -1,8 +1,11 @@
-from calc_statistics import amount_of_sentences
+from calc_statistics import amount_of_sentences, amount_of_non_declarative_sentences
+
+TEST_STR = 'Hello, man! User.com wants to say that he died... Can u call me?! '
 
 
 def main():
-    print(amount_of_sentences("Hello, man! User.com wants to say that he died... Can u call me?! "))
+    print(amount_of_sentences(TEST_STR))
+    print(amount_of_non_declarative_sentences(TEST_STR))
 
 
 if __name__ == '__main__':
