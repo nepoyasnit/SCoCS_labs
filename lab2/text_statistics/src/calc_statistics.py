82dc99881b3a51da736330cf8cc3009e25de0fcd
Maxim735 <maksiksay@gmail.com>
2023-02-21 23:38:31 +0300

    add average length counters

--- lab2/text_statistics/src/calc_statistics.py
+++ lab2/text_statistics/src/calc_statistics.py
@@ -10,3 +10,34 @@ def amount_of_non_declarative_sentences(text: str) -> int:
     regex = r'([!?] )'
     return len([*re.finditer(regex, text)])
 
+
+def average(numbers: list[float]) -> float:
+    if not numbers:
+        return 0
+
+    return sum(numbers) / len(numbers)
+
+
+def extract_words(text: str) -> list[str]:
+    regex = f'\w*[a-zA-Z]\w*'
+    words = re.findall(regex, text)
+    return words
+
+
+def contain_words(text: str) -> bool:
+    return bool(extract_words(text))
+
+
+def average_len_of_sentences(text: str) -> float:
+    regex = r'([.!?] )'
+    sentences = filter(contain_words, re.split(regex, text))
+    return average(list(map(lambda word: sum(len_of_words(word)), sentences)))
+
+
+def len_of_words(text: str) -> list[int]:
+    words = extract_words(text)
+    return list(map(lambda word: len(word), words))
+
+
+def average_len_of_words(text: str) -> float:
+    return average(len_of_words(text))
--- lab2/text_statistics/src/main.py
+++ lab2/text_statistics/src/main.py
@@ -1,4 +1,5 @@
-from calc_statistics import amount_of_sentences, amount_of_non_declarative_sentences
+from calc_statistics import amount_of_sentences, amount_of_non_declarative_sentences, \
+    average_len_of_words, average_len_of_sentences
 
 TEST_STR = 'Hello, man! User.com wants to say that he died... Can u call me?! '
 
@@ -6,6 +7,8 @@ TEST_STR = 'Hello, man! User.com wants to say that he died... Can u call me?! '
 def main():
     print(amount_of_sentences(TEST_STR))
     print(amount_of_non_declarative_sentences(TEST_STR))
+    print(average_len_of_words(TEST_STR))
+    print(average_len_of_sentences(TEST_STR))
 
 
 if __name__ == '__main__':
