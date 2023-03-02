d516fd8939f82940cb50cd6077afbb28f5aed38d
Maxim735 <maksiksay@gmail.com>
2023-03-02 23:26:46 +0300

    fix some mistakes

--- lab1/src/even_finder.py
+++
@@ -1,8 +0,0 @@
-def find_even(numbers: list):
-    even_numbers = []
-
-    for element in numbers:
-        if element % 2 == 0:
-            even_numbers.append(element)
-
-    return even_numbers
