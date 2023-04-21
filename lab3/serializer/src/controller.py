4f83ad82ef7034a905e8b6ed8a8d1dd8b6827dfe
Maxim735 <maksiksay@gmail.com>
2023-04-22 00:44:15 +0300

    add serialize function for primitives

--- lab3/serializer/src/controller.py
+++ lab3/serializer/src/controller.py
@@ -1,4 +1,6 @@
-from constants import JSON_DATA_TYPE, TOML_DATA_TYPE, YAML_DATA_TYPE
+from constants import JSON_DATA_TYPE, TOML_DATA_TYPE, \
+    YAML_DATA_TYPE, UNKNOWN_TYPE_ERROR
+from json_serializer import JsonSerializer
 
 
 class Controller:
@@ -6,7 +8,7 @@ class Controller:
 
     def __init__(self, data_format):
         if data_format == JSON_DATA_TYPE:
-            # self.serializer = JsonSerializer()
+            self.serializer = JsonSerializer()
             pass
 
         elif data_format == YAML_DATA_TYPE:
@@ -18,4 +20,4 @@ class Controller:
             # self.serializer = TomlSerializer()
 
         else:
-            raise Exception('Unknown type of serialization!')
+            raise Exception(UNKNOWN_TYPE_ERROR)
--- lab3/serializer/src/json_serializer.py
+++ lab3/serializer/src/json_serializer.py
@@ -1,10 +1,13 @@
+from constants import PRIMITIVE_TYPES
 
 
 class JsonSerializer:
     _current_position: int
+    _indent: int
 
     def __init__(self):
         self._current_position = 0
+        self._indent = 0
 
     def load(self, file):
         return self.loads(file.read())
@@ -16,4 +19,34 @@ class JsonSerializer:
         file.write(self.dumps(obj))
 
     def dumps(self, obj):
+        return self._convert_to_json_str(obj)
+
+    def _convert_to_json_str(self, obj):
+        if isinstance(obj, PRIMITIVE_TYPES):
+            return self._serialize_primitives(obj)
+
+        elif isinstance(obj, (list, set, tuple)):
+            return self._serialize_collection(obj)
+
+    def _serialize_primitives(self, obj):
+        json_string = '\n' + ' ' * self._indent + '{\n'
+        json_string += ' ' * self._indent + f'"type": "{type(obj).__name__}"\n'
+        json_string += ' ' * self._indent + f'"value": '
+
+        if obj is None:
+            json_string += 'null'
+        elif isinstance(obj, bool):
+            json_string += 'true' if obj else 'false'
+        elif isinstance(obj, (int, float)):
+            json_string += str(obj)
+        elif isinstance(obj, str):
+            json_string += f'"{obj}"'
+
+        json_string += '\n' + ' ' * self._indent + '}'
+        return json_string
+
+    def _serialize_collection(self, obj):
+        pass
+
+    def _serialize_dict(self, obj):
         pass
