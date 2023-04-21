90c6aa8261546f442bd8e6c0d6380a0b177715c5
Maxim735 <maksiksay@gmail.com>
2023-04-22 00:44:53 +0300

    edit constants

--- lab3/serializer/src/constants.py
+++ lab3/serializer/src/constants.py
@@ -3,3 +3,7 @@ JSON_DATA_TYPE = '.json'
 TOML_DATA_TYPE = '.toml'
 
 YAML_DATA_TYPE = '.yaml'
+
+PRIMITIVE_TYPES = (int, str, bool, float, type(None))
+
+UNKNOWN_TYPE_ERROR = 'Unknown type of serialization!'
